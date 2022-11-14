7343022f5267f62fa6b6200d27f6340acbaf2a80
ninolis <97913013+ninolis@users.noreply.github.com>
2022-11-14 20:53:14 +0200

    Create theory-autochecking.js

---
+++ js/theory-autochecking.js
@@ -0,0 +1,71 @@
+'use strict';
+
+
+///////////////////////////////////////////////
+// JS V2 block-2 Autochecking  ASSIGNMENT 1/41  Status: Done
+
+/*
+** THEORY
+
+
+** TASK
+
+
+** Test
+
+
+ */
+
+// Javascript Editor:
+// START
+/*
+
+*/
+
+// Javascript Editor:
+// FINISH
+/*
+
+*/
+
+// RESULT
+/*
+
+
+*/
+
+
+
+
+///////////////////////////////////////////////
+// JS V2 block-2 Autochecking  ASSIGNMENT 2/41  Status: Done
+
+/*
+** THEORY
+
+
+** TASK
+
+
+** Test
+
+
+ */
+
+// Javascript Editor:
+// START
+/*
+
+*/
+
+// Javascript Editor:
+// FINISH
+/*
+
+*/
+
+// RESULT
+/*
+
+
+*/
